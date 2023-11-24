614c08e4598da05a42060a29cd187278bf71037a
Alex <alex@alex-dev.me>
2023-11-24 15:38:37 -0600

    Refactor form inputs in SimpleForm component

--- src/02-useEffect/SimpleForm.jsx
+++ src/02-useEffect/SimpleForm.jsx
@@ -36,73 +36,28 @@ const SimpleForm = () => {
     <>
       <h1>Simple Form</h1>
       <hr />
-      <form action="">
-        <label htmlFor="username" className="form-label mt-2 mb-0 pb-0 d-block">
-          Enter the username you want to use
-        </label>
-        <input
-          type="text"
-          className="form-control mt-2"
-          placeholder="Username"
-          name="username"
-          value={username}
-          onChange={onInputChange}
-        />
-        <label htmlFor="email" className="form-label mt-2 mb-0 pb-0 d-block">
-          Enter your email
-        </label>
-        <input
-          type="email"
-          name="email"
-          className="form-control mt-2"
-          placeholder="Email"
-          value={email}
-          onChange={onInputChange}
-        />
-        <label htmlFor="name" className="form-label mt-2 mb-0 pb-0 d-block">
-          Enter your name
-        </label>
-        <input
-          type="text"
-          name="name"
-          className="form-control mt-2"
-          placeholder="Name"
-        />
-        <label htmlFor="lastname" className="form-label mt-2 mb-0 pb-0 d-block">
-          Enter your lastname
-        </label>
-        <input
-          type="text"
-          name="lastname"
-          className="form-control mt-2"
-          placeholder="Lastname"
-        />
-        <label htmlFor="password" className="form-label mt-2 mb-0 pb-0 d-block">
-          Enter your password
-        </label>
-        <input
-          type="password"
-          name="password"
-          className="form-control mt-2"
-          placeholder="Password"
-        />
-        <label
-          htmlFor="password2"
-          className="form-label mt-2 mb-0 pb-0 d-block"
-        >
-          Confirm your password
-        </label>
-        <input
-          type="password"
-          name="password2"
-          className="form-control mt-2"
-          placeholder="Confirm password"
-        />
-        <hr />
-        <button type="submit" className="btn btn-primary mt-2">
-          Sign Up
-        </button>
-      </form>
+      <label htmlFor="username" className="form-label mt-2 mb-0 pb-0 d-block">
+        Enter your username
+      </label>
+      <input
+        type="text"
+        className="form-control mt-2"
+        placeholder="Username"
+        name="username"
+        value={username}
+        onChange={onInputChange}
+      />
+      <label htmlFor="email" className="form-label mt-2 mb-0 pb-0 d-block">
+        Enter your email
+      </label>
+      <input
+        type="email"
+        name="email"
+        className="form-control mt-2"
+        placeholder="Email"
+        value={email}
+        onChange={onInputChange}
+      />
     </>
   );
 };
