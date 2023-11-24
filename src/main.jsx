75478ab919469e963a6b2821627a0da05b30d314
Alex <alex@alex-dev.me>
2023-11-24 11:52:41 -0600

    Add CounterWithCustomHook component

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import "./scss/styles.scss";
 import * as bootstrap from "bootstrap";
 import HooksApp from "./HooksApp";
 import CounterApp from "./01-useState/CounterApp";
+import CounterWithCustomHook from "./01-useState/CounterWithCustomHook";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <HooksApp /> */}
-    <CounterApp />
+    {/* <CounterApp /> */}
+    <CounterWithCustomHook />
   </React.StrictMode>
 );
