cf0c66b1ef37646644e2e808abefdb1733dc32f4
Alex <alex@alex-dev.me>
2023-11-24 11:52:28 -0600

    Add CounterWithCustomHook component

---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,40 @@
+import useCounter from "../hooks/useCounter";
+
+const CounterWithCustomHook = () => {
+  const { counter, increment, decrement, reset } = useCounter();
+  return (
+    <>
+      <div className="container text-center">
+        <h1 className="display-4">Counter with Hook: {counter}</h1>
+      </div>
+      <hr />
+      <div className="container text-center">
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-warning"
+            onClick={() => decrement()}
+          >
+            -1
+          </button>
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={() => reset()}
+          >
+            Reset
+          </button>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => increment()}
+          >
+            +1
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CounterWithCustomHook;
