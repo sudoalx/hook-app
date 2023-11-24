e13446407fd9ee26045432d25180d753da814f3a
Alex <alex@alex-dev.me>
2023-11-24 09:53:49 -0600

    Refactor useState in CounterApp.jsx pass state with spread operator

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
 const CounterApp = () => {
-  const [{ counter1, counter2, counter3 }, setCounter] = useState({
+  const [state, setCounter] = useState({
     counter1: 10,
     counter2: 20,
     counter3: 30,
   });
+  const { counter1, counter2, counter3 } = state;
   return (
     <>
       <h1>Counter: {counter1}</h1>
@@ -31,9 +32,8 @@ const CounterApp = () => {
         className="btn btn-success"
         onClick={() =>
           setCounter({
+            ...state,
             counter1: counter1 + 1,
-            counter2: counter2 + 1,
-            counter3: counter3 + 1,
           })
         }
       >
