303256c65e4f4e5d24fe109209a8770fe831f96a
Alex <alex@alex-dev.me>
2023-11-28 13:44:43 -0600

    Add CustomHookForm component

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import HooksApp from "./HooksApp";
 import CounterApp from "./01-useState/CounterApp";
 import CounterWithCustomHook from "./01-useState/CounterWithCustomHook";
 import SimpleForm from "./02-useEffect/SimpleForm";
+import CustomHookForm from "./02-useEffect/CustomHookForm";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <HooksApp /> */}
     {/* <CounterApp /> */}
     {/* <CounterWithCustomHook /> */}
-    <SimpleForm />
+    {/* <SimpleForm /> */}
+    <CustomHookForm />
   </React.StrictMode>
 );
