f885efdc3e3b79d96f70abd4345a5e992dcd8a77
Alex <alex@alex-dev.me>
2023-11-30 10:13:10 -0600

    Add FocusScreen component

---
+++ src/04-useRef/FocusScreen.jsx
@@ -0,0 +1,10 @@
+export const FosusScreen = () => {
+  return (
+    <>
+      <h1>Focus Screen</h1>
+      <hr />
+      <input type="text" className="form-control" placeholder="Your name" />
+      <button className="btn btn-outline-primary mt-5">Focus</button>
+    </>
+  );
+};
