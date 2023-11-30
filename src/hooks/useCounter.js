a142b070990f2ed764ddf5920ff8f93b0d960fcf
Alex <alex@alex-dev.me>
2023-11-30 02:18:36 -0600

    Add maxValue parameter to useCounter hook

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,13 +1,16 @@
 import { useState } from "react"
 
-const useCounter = (initialValue = 10) => {
+const useCounter = (initialValue = 1, maxValue = 10) => {
     const [counter, setCounter] = useState(initialValue)
     const increment = (value = 1) => {
         setCounter(counter + value)
+        if (counter + value > maxValue) {
+            reset()
+        }
     }
     const decrement = (value = 1) => {
         setCounter(counter - value)
-        if (counter - value < 1) {
+        if (counter - value < initialValue) {
             reset()
         }
     }
