f03aa907728e76184411fc84d4886d5db60a14b1
Alex <alex@alex-dev.me>
2023-11-30 03:20:24 -0600

    Add SkipValues component with PropTypes

---
+++ src/03-examples/components/SkipValues.jsx
@@ -0,0 +1,44 @@
+import PropTypes from "prop-types";
+export const CustomValues = ({
+  increaseValue,
+  decreaseValue,
+  setIncreaseValue,
+  setDecreaseValue,
+}) => {
+  return (
+    <div className="d-grid gap-1 m-1 text-center">
+      <label htmlFor="decrementBy" className="form-label">
+        Skip Forward
+      </label>
+      <input
+        type="number"
+        className="form-control text-center"
+        id="decrementBy"
+        value={increaseValue}
+        onChange={(e) => {
+          setIncreaseValue(e);
+        }}
+      />
+
+      <label htmlFor="incrementBy" className="form-label">
+        Skip Backward
+      </label>
+      <input
+        type="number"
+        className="form-control text-center"
+        id="incrementBy"
+        value={decreaseValue}
+        onChange={(e) => {
+          setDecreaseValue(e);
+        }}
+      />
+    </div>
+  );
+};
+
+CustomValues.propTypes = {
+  increaseValue: PropTypes.number.isRequired,
+  decreaseValue: PropTypes.number.isRequired,
+  setIncreaseValue: PropTypes.func.isRequired,
+  setDecreaseValue: PropTypes.func.isRequired,
+};
