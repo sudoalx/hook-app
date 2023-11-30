5783982fcabfd4bf189f01c9869d6b356eae9b7d
Alex <alex@alex-dev.me>
2023-11-30 03:20:07 -0600

    Add Card component with propTypes

---
+++ src/03-examples/components/Card.jsx
@@ -0,0 +1,80 @@
+import PropTypes from "prop-types";
+import { CustomValues } from "./SkipValues";
+import { useState } from "react";
+
+export const Card = ({
+  image,
+  name,
+  species,
+  decrement,
+  increment,
+  counter,
+  isLoading,
+}) => {
+  const [increaseValue, setIncreaseValue] = useState(1);
+  const [decreaseValue, setDecreaseValue] = useState(1);
+  const handleDeacreaseValue = (e) => {
+    e.target.value > 0 && setDecreaseValue(parseInt(e.target.value));
+  };
+  const handleIncreaseValue = (e) => {
+    e.target.value > 0 && setIncreaseValue(parseInt(e.target.value));
+  };
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center flex-column"
+      style={{ minHeight: "80vh" }}
+    >
+      <div className="card text-center mx-auto">
+        <img src={image} className="card-img-top img-fluid" alt={name} />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">{species}</p>
+        </div>
+        <div className="d-grid gap-1 m-1">
+          <button
+            className="btn btn-primary"
+            type="button"
+            disabled={isLoading}
+            onClick={() => decrement(decreaseValue)}
+          >
+            Previous
+          </button>
+          <button
+            className="btn btn-primary"
+            type="button"
+            disabled={isLoading}
+            onClick={() => increment(increaseValue)}
+          >
+            Next
+          </button>
+          <label htmlFor="counter" className="form-label">
+            Current character:
+          </label>
+          <input
+            type="number"
+            className="form-control text-center"
+            id="counter"
+            value={counter}
+            readOnly
+          />
+        </div>
+      </div>
+      <CustomValues
+        decreaseValue={decreaseValue}
+        increaseValue={increaseValue}
+        setDecreaseValue={handleDeacreaseValue}
+        setIncreaseValue={handleIncreaseValue}
+      />
+    </div>
+  );
+};
+
+Card.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  species: PropTypes.string.isRequired,
+  decrement: PropTypes.func.isRequired,
+  increment: PropTypes.func.isRequired,
+  counter: PropTypes.number.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
