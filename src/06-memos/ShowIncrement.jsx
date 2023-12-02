c3ecf82bc4b3a2ef73d1f8c92cfadd29b6e19283
Alex <alex@alex-dev.me>
2023-12-02 03:33:40 -0600

    I forgot to add memo that's why it didn't work before

--- src/06-memos/ShowIncrement.jsx
+++ src/06-memos/ShowIncrement.jsx
@@ -1,18 +1,19 @@
+import React from "react";
 import PropTypes from "prop-types";
 
-export const ShowIncrement = ({ increment }) => {
+export const ShowIncrement = React.memo(({ increment }) => {
   console.log("ShowIncrement rendered");
   return (
     <button
       className="btn btn-primary"
       onClick={() => {
-        increment();
+        increment(5);
       }}
     >
       +1
     </button>
   );
-};
+});
 
 ShowIncrement.propTypes = {
   increment: PropTypes.func.isRequired,
