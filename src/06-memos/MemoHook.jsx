924d5ef1c97188c7dc8f8b9a95e6d4b69a74140f
Alex <alex@alex-dev.me>
2023-12-02 03:22:40 -0600

    Add MemoHook component

---
+++ src/06-memos/MemoHook.jsx
@@ -0,0 +1,35 @@
+import useCounter from "../hooks/useCounter";
+import { useMemo, useState } from "react";
+
+const heavyStuff = (interationNum = 100) => {
+  for (let i = 0; i < interationNum; i++) {
+    console.log("Here we go...");
+  }
+  return `Iterations: ${interationNum}`;
+};
+
+export const MemoHook = () => {
+  const { counter, increment } = useCounter(40);
+  const [show, setShow] = useState(true);
+
+  const memoValue = useMemo(() => heavyStuff(counter), [counter]);
+  return (
+    <>
+      <h1>
+        Counter:<small> {counter} </small>
+      </h1>
+      <hr />
+      <h4>{memoValue}</h4>
+      <button className="btn btn-primary" onClick={() => increment()}>
+        +1
+      </button>
+
+      <button
+        className="btn btn-outline-primary"
+        onClick={() => setShow(!show)}
+      >
+        Show/Hide {JSON.stringify(show)}
+      </button>
+    </>
+  );
+};
