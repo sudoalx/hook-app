5ef476a8757e7bc2ca3a2b4fb2b813db5399dfc6
Alex <alex@alex-dev.me>
2023-12-04 09:56:18 -0600

    Update TodoApp UI

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -19,12 +19,41 @@ export const TodoApp = () => {
   return (
     <>
       <h1>TodoApp</h1>
-      <hr />
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-      </ul>
+      <div className="row">
+        <div className="col-7">
+          <h4 className="text-start">Todo: 5</h4>
+          <hr />
+        </div>
+        <div className="col-5">
+          <h4 className="text-end">Done: 5</h4>
+          <hr />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item d-flex justify-content-between align-items-center bg-dark text-white border border-white rounded">
+              <span>Item 1</span>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Add TODO</h4>
+          <form>
+            <input
+              type="text"
+              name="todo"
+              className="form-control mb-2 bg-dark text-white"
+              autoComplete="off"
+            />
+            <button type="submit" className="btn btn-outline-primary mt-1">
+              Add
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
