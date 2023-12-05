c1b62325127721955015472ce0129a61fb0de682
Alex <alex@alex-dev.me>
2023-12-05 01:19:45 -0600

    Refactor todoReducer to add and remove todos

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -1,8 +1,10 @@
-export const todoReducer = (initialState = [], action) => {
+export const todoReducer = (initialState, action) => {
     switch (action.type) {
-        case 'ABC':
+        case '[TODO] Add Todo':
+            return [...initialState, action.payload]
 
-            throw new Error('Action.type = ABC no esta implementada')
+        case '[TODO] Remove Todo':
+            return initialState.filter(todo => todo.id !== action.payload)
 
         default:
             return initialState
