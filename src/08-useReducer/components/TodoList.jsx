45894ef814a13140d35128eb20f8c97eb0dc8ca8
Alex <alex@alex-dev.me>
2023-12-05 01:20:33 -0600

    Add onRemoveTodo prop to TodoList component

--- src/08-useReducer/components/TodoList.jsx
+++ src/08-useReducer/components/TodoList.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 import { TodoItem } from "./TodoItem";
-export const TodoList = ({ todos }) => {
+export const TodoList = ({ todos, onRemoveTodo }) => {
   return (
     <ul className="list-group">
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem key={todo.id} todo={todo} onRemoveTodo={onRemoveTodo} />
       ))}
     </ul>
   );
@@ -12,4 +12,5 @@ export const TodoList = ({ todos }) => {
 
 TodoList.propTypes = {
   todos: PropTypes.array.isRequired,
+  onRemoveTodo: PropTypes.func.isRequired,
 };
