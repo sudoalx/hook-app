fd5c403f75fd97d5fc5722183dfb1b82fad63924
Alex <alex@alex-dev.me>
2023-12-05 01:20:23 -0600

    Add onRemoveTodo functionality to TodoItem component

--- src/08-useReducer/components/TodoItem.jsx
+++ src/08-useReducer/components/TodoItem.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-export const TodoItem = ({ todo }) => {
+export const TodoItem = ({ todo, onRemoveTodo }) => {
   const { id, description } = todo;
 
   return (
@@ -9,11 +9,17 @@ export const TodoItem = ({ todo }) => {
       className="list-group-item d-flex justify-content-between align-items-center bg-dark text-white"
     >
       <span>{description}</span>
-      <button className="btn btn-danger">Delete</button>
+      <div className="btn-group">
+        <button className="btn btn-success">🗸</button>
+        <button className="btn btn-danger" onClick={() => onRemoveTodo(id)}>
+          ⛌
+        </button>
+      </div>
     </li>
   );
 };
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
+  onRemoveTodo: PropTypes.func.isRequired,
 };
