682f586f4b7e8f049d0d5327258d2683d0b5dcab
Alex <alex@alex-dev.me>
2023-12-06 02:45:01 -0600

    Fix default value in useCounter hook

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types"
 import { useState } from "react"
 
-export const useCounter = (initialValue, maxValue) => {
+export const useCounter = (initialValue = 1, maxValue) => {
     const [counter, setCounter] = useState(initialValue)
     const increment = (value = 1) => {
         setCounter(counter + value)
