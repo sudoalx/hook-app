b9711017b5f7c969113a78765f8c3389c5f6593c
Alex <alex@alex-dev.me>
2023-12-05 21:04:30 -0600

    Add FourOhFour component

---
+++ src/09-useContext/FourOhFour.jsx
@@ -0,0 +1,8 @@
+export const FourOhFour = () => {
+  return (
+    <div className="container text-center">
+      <h1 className="display-1">404</h1>
+      <p className="lead">Page not found</p>
+    </div>
+  );
+};
