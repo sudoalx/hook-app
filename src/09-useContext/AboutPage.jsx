a0a443b00ee445ba82eef1f77752aea359a2ae51
Alex <alex@alex-dev.me>
2023-12-06 02:44:35 -0600

    Update AboutPage content

--- src/09-useContext/AboutPage.jsx
+++ src/09-useContext/AboutPage.jsx
@@ -1,8 +1,30 @@
 export const AboutPage = () => {
   return (
     <>
-      <h1>AboutPage</h1>
+      <h1>About this app</h1>
       <hr />
+      <div className="container">
+        <p>This is a demo app to show the use of React Hooks.</p>
+        <p>The app was created using:</p>
+        <ul>
+          <li>React</li>
+          <li>React Router</li>
+          <li>Bootstrap</li>
+          <li>Vite</li>
+        </ul>
+        <p>Design was not a priority so it's not the best.</p>
+      </div>
+      <div className="lead text-start fst-italic">
+        Source:{" "}
+        <a
+          className="text-decoration-none"
+          href="https://github.com/sudoalx/hook-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </div>
     </>
   );
 };
