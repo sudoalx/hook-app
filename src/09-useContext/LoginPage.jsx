c727be7c83cde2940de490532863ce2b2e4b0c6d
Alex <alex@alex-dev.me>
2023-12-06 00:18:36 -0600

    Add user context and set user data

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,8 +1,22 @@
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
+
 export const LoginPage = () => {
+  const { user, setUser } = useContext(UserContext);
+
   return (
     <>
       <h1>LoginPage</h1>
       <hr />
+      <pre>{JSON.stringify(user, null, 3)}</pre>
+      <button
+        className="btn btn-primary"
+        onClick={() =>
+          setUser({ uuid: 123, name: "Alex", email: "alex@alex-dev.me" })
+        }
+      >
+        Set user
+      </button>
     </>
   );
 };
