758bc6522ab1a90fa2654b55b1eb0338685d884e
Alex <alex@alex-dev.me>
2023-12-06 02:44:52 -0600

    Add Footer component

---
+++ src/09-useContext/components/Footer.jsx
@@ -0,0 +1,17 @@
+export const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3">
+      <div className="container text-center">
+        <span className="text-center text-white small mb-0">
+          &copy; 2023 -{" "}
+          <a
+            href="https://github.com/sudoalx"
+            className="text-decoration-none text-white"
+          >
+            sudoalx
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
+};
