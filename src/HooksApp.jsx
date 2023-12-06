228ea6716ac73eae6f35bd027e47d13b350f1d23
Alex <alex@alex-dev.me>
2023-12-06 02:43:41 -0600

    Refactor HooksApp component

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-
 export const HooksApp = () => {
   return (
-    <div>
-      <h1>Hooks App</h1>
-      <p className="lead">This is a React app that uses hooks.</p>
-    </div>
+    <>
+      <p className="lead">
+        This is a React app that demos the use of React Hooks. Source code is at{" "}
+        <a href="https://github.com/sudoalx/hook-app">GitHub</a>
+      </p>
+      <hr />
+    </>
   );
 };
