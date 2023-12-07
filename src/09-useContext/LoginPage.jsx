583f13ebd463745e11e7d7fd45671a094f31e8e3
Alex <alex@alex-dev.me>
2023-12-07 12:39:58 -0600

    Fix aria-label in LoginPage component

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -8,12 +8,13 @@ export const LoginPage = () => {
     <>
       <h1>LoginPage</h1>
       <hr />
-      <pre>{JSON.stringify(user, null, 3)}</pre>
+      <pre aria-label="pre">{JSON.stringify(user, null, 3)}</pre>
       <button
         className="btn btn-primary"
         onClick={() =>
           setUser({ uuid: 123, name: "Alex", email: "alex@alex-dev.me" })
         }
+        aria-label="set-user"
       >
         Set user
       </button>
