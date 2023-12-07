ddbdbe4fb71bf929ab9bcc2f55ec8016bad3c9db
Alex <alex@alex-dev.me>
2023-12-06 19:49:57 -0600

    Add fireEvent and useCounter to MultipleCustomHooks.test.js

--- tests/03-examples/MultipleCustomHooks.test.js
+++ tests/03-examples/MultipleCustomHooks.test.js
@@ -1,21 +1,33 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import MultipleCustomHooks from "../../src/03-examples/MultipleCustomHooks"
-import { useFetch } from "../../src/hooks"
+import { useCounter, useFetch } from "../../src/hooks"
 
 jest.mock('../../src/hooks/useFetch')
+jest.mock('../../src/hooks/useCounter')
 
 describe('tests on <MultipleCustomHooks/>', () => {
+    const mockIncrement = jest.fn()
+    useCounter.mockReturnValue({
+        counter: 1,
+        increment: mockIncrement
+    })
+
+    beforeAll(() => {
+        jest.clearAllMocks()
+    })
+
     test('should show the default component', () => {
         useFetch.mockReturnValue({
             data: null,
             isLoading: true,
-            error: null
+            hasError: null
         })
         render(<MultipleCustomHooks />)
 
         expect(screen.getByText('Loading...'))
         expect(screen.getByText('Rick and Morty Characters'))
     })
+
     test('should return data from API', () => {
         useFetch.mockReturnValue({
             data: { name: "Alex", species: "Human" },
@@ -23,8 +35,22 @@ describe('tests on <MultipleCustomHooks/>', () => {
             hasError: null
         })
         render(<MultipleCustomHooks />)
-        screen.debug()
+
         expect(screen.getByText("Alex")).toBeTruthy()
         expect(screen.getByText("Human")).toBeTruthy()
+    })
+
+    test('should return data from API', () => {
+        useFetch.mockReturnValue({
+            data: { name: "Alex", species: "Human" },
+            isLoading: false,
+            hasError: null
+        })
+
+        render(<MultipleCustomHooks />)
+        const nextButton = screen.getByRole('button', { name: "Next" })
+        fireEvent.click(nextButton)
+
+        expect(mockIncrement).toHaveBeenCalled()
     })
 })
